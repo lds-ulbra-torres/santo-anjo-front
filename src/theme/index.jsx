a46adfff571b22980234df08dd72e96dce85e588
quevicente <viceents@gmail.com>
2019-11-04 22:47:17 -0300

    added some colors to main theme

---
+++ src/theme/index.jsx
@@ -0,0 +1,26 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+// import { colors } from './colors';
+
+export const theme = createMuiTheme({
+    typography: {
+        fontFamily: ['Roboto', 'sans-serif']
+    },
+    palette: {
+        primary: {
+
+        },
+        secondary: {
+
+        },
+        text: {
+            primary: '#777777',
+        },
+        background: {
+            default: '#F1F5F6',
+            verticalMenu: '#E5E5E5'
+        },
+        header: {
+            default: '#00A9DF'
+        }
+    }
+});
