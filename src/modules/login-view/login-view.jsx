1aa84e27488af56063a5b68386fd52c2b7f1bc27
quevicente <viceents@gmail.com>
2019-11-06 20:02:30 -0300

    push new feature

--- src/modules/login-view/login-view.jsx
+++ src/modules/login-view/login-view.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import { Button, Input } from '@material-ui/core'
 import Logo from '../../assets/Logo.svg'
 
 export class LoginView extends Component {
     render() {
+
         return (
-            <div className={styles.container}>
-                <img src={Logo} />
+            <Title>
+                UHAUDHSU
+                {/* <img src={Logo} />
                 <h1>Sistema de Água Santo Anjo da Guarda</h1>
                 <Input></Input>
                 <Input></Input>
-                <Button>Entrar</Button>
-            </div>
+                <Button>Entrar</Button> */}
+            </Title>
         );
     };
 };
 
-const styles = {
-    container: {
-
-    }
-}
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
